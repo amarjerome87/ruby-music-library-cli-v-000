60f548291137e6d0c6cd36c71947c59782275935
Amar Chatman <amarjerome@gmail.com>
2018-06-19 17:10:08 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -96,10 +96,10 @@ def play_song
   end
   #.each do |song|
   #  if response == song.name
-
+  if song
      puts "Playing #{song.name} by #{song.artist.name}"
-   
-  #  end
+
+  end
   # end
 end
 
